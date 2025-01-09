f89b33faff2fc090311cc214d2033d46c166bafc
Erick Barros <erick@sensio.com.br>
2025-01-09 13:37:07 -0300

    fix(app.js): fixed cors error to put method

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const corsOptions = {
   origin: "http://localhost:5173",
-  methods: "GET, POST, DELETE",
+  methods: "GET, POST, PUT, DELETE",
   allowedHeaders: "Content-Type, Authorization",
 };
 
