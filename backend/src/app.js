499cdf4a2e98e00352a4d6f39050206b8e20dafa
Erick Barros <erick@sensio.com.br>
2025-01-09 11:29:32 -0300

    refactor(app.js): updated cors validations

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,13 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 
 routes(app);
 
