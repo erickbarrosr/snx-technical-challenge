3c8fc1c9f20da40614c5733a36efcd6f6acfb8f4
Erick Barros <erick@sensio.com.br>
2025-01-09 13:38:07 -0300

    feat(postRoutes.js): added route to getPostById

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -5,6 +5,7 @@ const authMiddleware = require("../middlewares/authMiddleware.js");
 const router = express.Router();
 
 router.get("/posts", authMiddleware, postController.getPosts);
+router.get("/posts/:id", authMiddleware, postController.getPostById);
 router.post("/posts", authMiddleware, postController.createPost);
 router.put("/posts/:id", authMiddleware, postController.updatePost);
 router.delete("/posts/:id", authMiddleware, postController.deletePost);
