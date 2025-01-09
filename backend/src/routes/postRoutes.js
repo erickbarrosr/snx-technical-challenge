9f41896ca4038a9c2a4c5063f20f3bb637ff069f
Erick Barros <erick@sensio.com.br>
2025-01-09 15:18:41 -0300

    feat(postRoutes.js): added route to getCommentsByPost

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -6,6 +6,11 @@ const router = express.Router();
 
 router.get("/posts", authMiddleware, postController.getPosts);
 router.get("/posts/:id", authMiddleware, postController.getPostById);
+router.get(
+  "/posts/:id/comments",
+  authMiddleware,
+  postController.getCommentsByPost
+);
 router.post("/posts", authMiddleware, postController.createPost);
 router.put("/posts/:id", authMiddleware, postController.updatePost);
 router.delete("/posts/:id", authMiddleware, postController.deletePost);
