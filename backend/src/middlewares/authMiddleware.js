8bff3931eb6967973a901d71fc99699c18fa61b5
Erick Barros <erick@sensio.com.br>
2025-01-09 11:31:54 -0300

    fix(authMiddleare.js): remvoe logs and update message validations

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Token not provided" });
+  if (!token) return res.status(401).json({ message: "Token não fornecido." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -16,7 +16,7 @@ const authMiddleware = (req, res, next) => {
     next();
   } catch (err) {
     console.error(err);
-    res.status(401).json({ message: "Invalid or expired token" });
+    res.status(401).json({ message: "Token inválido ou expirado." });
   }
 };
 
